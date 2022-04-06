1fdee74d3acb024795b6f4b4bbe24aad360c089a
Marcin Jedrecki <marcin.jedrecki@gmail.com>
2022-04-06 13:13:54 +0200

    Markdown fixes in README.md

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import random
-from pprint import pprint
 
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
+
 import APIHandler
 import html
 
@@ -16,10 +18,46 @@ class Ui(QtWidgets.QMainWindow):
         self.bg_style.buttonClicked.connect(self.styleChange)
         for obj in APIHandler.get_categories():
             self.categories.addItem(obj['name'])
+        self.categories.setCurrentIndex(0)
         self.getQuestionsLinkButton.clicked.connect(self.fetchquestions)
         self.questionTable.clicked.connect(self.questionSelector)
         self.qs_millionaire.setEnabled(False)  # gotta work this one out
+        self.actionReport_Issue_on_GitHub.triggered.connect(self.reportIssue)
+        self.actionExit.triggered.connect(lambda: self.close())
+        self.actionNew_Quiz.triggered.connect(self.newQuiz)
+        header = self.questionTable.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+        self.newQuiz()
         self.show()
+
+    def newQuiz(self):
+        self.getQuestionsLinkButton.setEnabled(True)
+        self.questionTable.setRowCount(0)
+        self.questionTable.setRowCount(0)
+        self.categories.setCurrentIndex(0)
+        self.categories.setEnabled(False)
+        self.qs_random.setChecked(True)
+        self.qs_custom.setChecked(False)
+        self.qt_any.setChecked(True)
+        self.qd_any.setChecked(True)
+        for rb1 in self.bg_type.buttons():
+            rb1.setEnabled(False)
+        for rb2 in self.bg_diff.buttons():
+            rb2.setEnabled(False)
+        self.amount.setValue(10)
+        self.bg_style.buttonClicked.connect(self.styleChange)
+        self.selectedQuestion.setText("")
+        hbox = QtWidgets.QHBoxLayout()
+        self.answerBox.setLayout(hbox)
+        for i in reversed(range(self.answerBox.layout().count())):
+            self.answerBox.layout().itemAt(i).widget().deleteLater()
+
+
+    def reportIssue(self):
+        url = QUrl("https://github.com/alucard87pl/EasyQuiz/issues")
+        QDesktopServices.openUrl(url)
 
     def fetchquestions(self):
         a = self.amount.value()
@@ -35,6 +73,7 @@ class Ui(QtWidgets.QMainWindow):
             question['userAnswer'] = None
             self.questionList.append(question)
         self.addQuestionstoTable()
+        self.getQuestionsLinkButton.setEnabled(False)
 
     def addQuestionstoTable(self):
         self.questionTable.setRowCount(0)
@@ -44,11 +83,9 @@ class Ui(QtWidgets.QMainWindow):
             self.questionTable.setItem(idx, 1, QtWidgets.QTableWidgetItem(self.questionList[idx]['difficulty']))
             self.questionTable.setItem(idx, 2, QtWidgets.QTableWidgetItem(self.questionList[idx]['type']))
             self.questionTable.setItem(idx, 3, QtWidgets.QTableWidgetItem(""))
-            pprint(self.questionList[idx])
 
     def styleChange(self):
         style = self.bg_style.checkedButton().text().lower()
-        print(style)
         if style == "random":
             self.categories.setCurrentIndex(0)
             self.categories.setEnabled(False)
@@ -94,14 +131,10 @@ class Ui(QtWidgets.QMainWindow):
         userAnswer = self.sender().text()
         self.questionList[self.questionTable.currentRow()]['userAnswer'] = userAnswer
         correctAnswer = self.questionList[self.questionTable.currentRow()]['correct_answer']
-        print("Answer: ", userAnswer)
-        print("Correct: ", correctAnswer)
         if userAnswer == correctAnswer:
-            print("Correct!")
             self.sender().setStyleSheet("background-color: #00FF00")
             self.questionTable.setItem(self.questionTable.currentRow(), 3, QtWidgets.QTableWidgetItem("Correct"))
         else:
-            print("Incorrect!")
             self.sender().setStyleSheet("background-color: #FF0000")
             self.questionTable.setItem(self.questionTable.currentRow(), 3, QtWidgets.QTableWidgetItem("Incorrect"))
         for i in reversed(range(self.answerBox.layout().count())):
